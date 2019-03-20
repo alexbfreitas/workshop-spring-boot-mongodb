3c74be34be48717e3cb1294f62ca2220ba37bff9
Alexandre Freitas <alexbfreitas@gmail.com>
2019-03-19 23:15:35 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/alexandrefreitas/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/alexandrefreitas/workshopmongo/repository/PostRepository.java
@@ -3,9 +3,9 @@ package com.alexandrefreitas.workshopmongo.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import com.alexandrefreitas.workshopmongo.domain.User;
+import com.alexandrefreitas.workshopmongo.domain.Post;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String> {
+public interface PostRepository extends MongoRepository<Post, String> {
 
 }
