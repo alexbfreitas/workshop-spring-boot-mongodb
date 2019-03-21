b49bd0cb2b883980f1b2bfe05921afa61294856c
Alexandre Freitas <alexbfreitas@gmail.com>
2019-03-20 22:16:32 -0300

    Obtendo um post por id

---
+++ src/main/java/com/alexandrefreitas/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.alexandrefreitas.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alexandrefreitas.workshopmongo.domain.Post;
+import com.alexandrefreitas.workshopmongo.repository.PostRepository;
+import com.alexandrefreitas.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+}
