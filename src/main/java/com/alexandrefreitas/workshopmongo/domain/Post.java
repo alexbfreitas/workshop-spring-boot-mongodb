88e1c15f1b399b61372e11613cad8848177c5060
Alexandre Freitas <alexbfreitas@gmail.com>
2019-03-24 17:17:49 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/alexandrefreitas/workshopmongo/domain/Post.java
+++ src/main/java/com/alexandrefreitas/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.alexandrefreitas.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.alexandrefreitas.workshopmongo.dto.AuthorDTO;
+import com.alexandrefreitas.workshopmongo.dto.CommentDTO;
 
 @Document(collection="post")
 public class Post implements Serializable{
@@ -18,6 +21,7 @@ public class Post implements Serializable{
 	private String title;
 	private String body;
 	private AuthorDTO authorDto;
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post( ) {
 		
@@ -72,6 +76,14 @@ public class Post implements Serializable{
 		this.authorDto = authorDto;
 	}	
 
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
