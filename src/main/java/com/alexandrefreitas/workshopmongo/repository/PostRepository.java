684294de80f2a126b84420534fc05700a7a8c195
Alexandre Freitas <alexbfreitas@gmail.com>
2019-03-24 17:50:55 -0300

    Consulta simples com @Query

--- src/main/java/com/alexandrefreitas/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/alexandrefreitas/workshopmongo/repository/PostRepository.java
@@ -4,6 +4,7 @@ package com.alexandrefreitas.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.alexandrefreitas.workshopmongo.domain.Post;
@@ -11,5 +12,8 @@ import com.alexandrefreitas.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	public List<Post> searchTitle(String text);
+	
 	public List<Post> findByTitleContainingIgnoreCase(String text);
 }
